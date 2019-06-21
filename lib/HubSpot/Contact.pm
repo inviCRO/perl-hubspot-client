1e636d4f2ff12fa2b0af57b53dee5d3f6f9bbf18
Ian Gibbs <ian.gibbs@virtualclarity.com>
2019-06-21 19:40:37 +0100

    Set values from JSON at build time

--- lib/HubSpot/Contact.pm
+++ lib/HubSpot/Contact.pm
@@ -28,12 +28,42 @@ This is the class returned by HubSpot::Client when it you execute methods that r
 =cut
 
 # Make us a class
-use subs qw(id primaryEmail firstName lastName company lastModifiedDateTime);
+use subs qw();
 use Class::Tiny qw(id primaryEmail firstName lastName company lastModifiedDateTime),
 {
 		# Default variables in here
 };
 use parent 'HubSpot::JSONBackedObject';
+
+sub BUILD
+{
+	my $self = shift;
+	
+	if($self->json)
+	{
+		$self->{'id'} = $self->json->{'vid'};
+		$self->{'firstName'} = $self->json->{'properties'}->{'firstname'}->{'value'};
+		$self->{'lastName'} = $self->json->{'properties'}->{'lastname'}->{'value'};
+		$self->{'company'} = $self->json->{'properties'}->{'company'}->{'value'};
+		$self->{'lastModifiedDateTime'} = DateTime->from_epoch(epoch => $self->json->{'properties'}->{'lastmodifieddate'}->{'value'}/1000, time_zone => 'UTC');
+		my $profiles = $self->json->{'identity-profiles'};
+		my $first_profile = $$profiles[0];						# other profiles may exist if it is a merged contact
+		my $identities = $first_profile->{'identities'};
+		my $found_email;
+		foreach my $identity (@$identities)
+		{
+			if($identity->{'is-primary'} && $identity->{'type'} eq "EMAIL")
+			{
+				$found_email = $identity->{'value'};
+			}
+		}
+		if($found_email)
+		{
+			$self->{'primaryEmail'} = $found_email;
+		}
+		# email isn't a compulsory field - might not be there
+	}
+}
 
 =pod
 
@@ -68,123 +98,27 @@ for example. Both do the same thing.
 
 The ID of the contact as it is known in HubSpot. You can use this to retrieve a specific contact only, rather than multiple contacts. See L<contact|HubSpot::Client/contact>
 
-=cut
-sub id
-{
-	my $self = shift;
-	
-	$self->{'id'} = $self->json->{'vid'};
-	
-	return $self->{'id'};
-}
-
-=pod
-
 =item firstName
 
 Returns the contact's first name, which is a mandatory field in HubSpot so this will always return something.
-
-=cut
-sub firstName
-{
-	my $self = shift;
-	
-	$self->{'firstName'} = $self->json->{'properties'}->{'firstname'}->{'value'};
-	
-	return $self->{'firstName'};
-}
-
-=pod
 
 =item lastName
 
 Returns the contact's first name, which is a mandatory field in HubSpot so this will always return something.
 
-=cut
-sub lastName
-{
-	my $self = shift;
-	
-	$self->{'lastName'} = $self->json->{'properties'}->{'lastname'}->{'value'};
-	
-	return $self->{'lastName'};
-}
-
-=pod
-
 =item company
 
 Returns the contact's associated company name, if set, or B<undef> otherwise.
-
-=cut
-sub company
-{
-	my $self = shift;
-	
-	$self->{'company'} = $self->json->{'properties'}->{'company'}->{'value'};
-	
-	return $self->{'company'};
-}
-
-=pod
 
 =item lastModifiedDateTime
 
 Returns the date and time that the contact was last modified, as a L<DateTime>, Time zone is UTC, but you can use that module's methods to change that.
 
-=cut
-sub lastModifiedDateTime
-{
-	my $self = shift;
-	
-	$self->{'lastModifiedDateTime'} = DateTime->from_epoch(epoch => $self->json->{'properties'}->{'lastmodifieddate'}->{'value'}/1000, time_zone => 'UTC');
-	
-	return $self->{'lastModifiedDateTime'};
-}
-
-=pod
-
 =item primaryEmail
 
 Returns the email address set as the primary email, if set, or B<undef> otherwise.
 
-=cut
-sub primaryEmail
-{
-	my $self = shift;
-	
-	my $profiles = $self->json->{'identity-profiles'};
-	my $first_profile = $$profiles[0];						# other profiles may exist if it is a merged contact
-	my $identities = $first_profile->{'identities'};
-	my $found_email;
-	foreach my $identity (@$identities)
-	{
-		if($identity->{'is-primary'} && $identity->{'type'} eq "EMAIL")
-		{
-			$found_email = $identity->{'value'};
-		}
-	}
-	if($found_email)
-	{
-		$self->{'primaryEmail'} = $found_email;
-	}
-	# email isn't a compulsory field - might not be there
-	
-	return $self->{'primaryEmail'};
-}
-
-#~ sub name
-#~ {
-	#~ my $self = shift;
-	
-	#~ return $self->firstName." ".$self->lastName;
-#~ }
-
-
-=pod
-
 =back
 
 =cut
-
 1;
